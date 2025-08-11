46465a6f539307486f1ea7bf25183e0bc0d23f13
murayabrian <brian.muraya@agencify.insure>
2025-08-11 07:24:08 +0300

    Structured project and added required claases and controler classes to begin with

---
+++ src/main/java/com/codewithbrian/springboot/SoftwareEngineerController.java
@@ -0,0 +1,9 @@
+package com.codewithbrian.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1Software-engineers")
+public class SoftwareEngineerController {
+}
