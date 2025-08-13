329b400485a301198d7b2ef732f8e39d2e5a1e14
murayabrian <brian.muraya@agencify.insure>
2025-08-13 20:04:52 +0300

    added a getEngineers request

--- src/main/java/com/codewithbrian/springboot/SoftwareEngineerController.java
+++ src/main/java/com/codewithbrian/springboot/SoftwareEngineerController.java
@@ -1,9 +1,20 @@
 package com.codewithbrian.springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/v1Software-engineers")
+@RequestMapping("api/v1software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(1, "James", "js, node, react, tailwindcss"),
+                new SoftwareEngineer(2, "Jamila", "java, spring, spring boot"),
+                new SoftwareEngineer(3, "Jayden", "python, appium, selenium, playwright"));
+    }
 }
