4050d6104e58399c8538497b48f436e23676c750
waititubrian <waititubrian9@gmail.com>
2026-02-05 06:59:55 +0300

    refactor

--- src/main/java/com/codewithbrian/springboot/SoftwareEngineerController.java
+++ src/main/java/com/codewithbrian/springboot/SoftwareEngineerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1software-engineers")
+@RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
     private final SoftwareEngineerService softwareEngineerService;
